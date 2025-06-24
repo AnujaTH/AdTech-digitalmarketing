d28f2e4b35bcdb8b75c80fc57e388f2227196263
AnujaTH <anuthileep20@gmail.com>
2025-06-24 15:45:08 +0530

    Add Changes In Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,12 +5,14 @@ import HowItWorks from "@/components/HowItWorks";
 import Pricing from "@/components/Pricing";
 import Footer from "@/components/Footer";
 import Chatbot from "@/components/Chatbot";
+
 // import AuthPage from "@/components/login";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Hero />
+    
       <About />
       {/* <CaseStudies /> */}
       <HowItWorks />
